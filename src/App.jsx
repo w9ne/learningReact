f7c5827cc503fa1d59c2b4b8ffaca4c5ddef2a3e
w9ne <jared119@gmail.com>
2026-05-27 00:06:12 -1000

    Create navbar and add imports to related files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
